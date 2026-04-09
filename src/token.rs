90ef49cd78bb4eec354128a63239278dc4a3d33d
Danna Sánchez <211834102+d-paola-schz@users.noreply.github.com>
2026-04-09 16:48:35 -0600

    Add Token and TokenType enums for lexer

---
+++ src/token.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType{
+  Keyword,
+  Variable,
+  String,
+  Number,
+  Operator,
+  Comments,
+  Whitespace,
+}
+
+#[derive(Debug, Clone)]
+struct Token{
+  pub text: String;
+  pub tokenType: TokenType,
+}
+
+impl Token {
+  pub fn new(text: String, tokenType: TokenType) -> Self {
+    Self {text, tokenType}
+  }
+}
