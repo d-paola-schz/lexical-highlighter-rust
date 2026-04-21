eea2431fd1c38568333102338e80bce3a914e615
danna-schz <dpaola.schz@gmail.com>
2026-04-20 18:20:37 -0600

    fixed syntax error and test case updated

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,86 +3,62 @@ use crate::token::{Token, TokenType};
 // #[derive(Debug, Clone)]
 pub struct Lexer{
     input: Vec<char>,
-    currentPos: usize,
+    current_pos: usize,
 }
 
 impl Lexer{
     pub fn new(input: String) -> Self{
         Self{
             input: input.chars().collect(),
-            currentPos: 0,
+            current_pos: 0,
         }
     }
-
-    pub fn tokenize(&mut self) -> Vec<Token> {
-        let mut tokens = Vec::new();
-
-        while let Some(ch) = self.current(){
-            if ch.is_whitespace(){
-                tokens.push(self.readWhitespace());
-            } else if ch == '#' {
-                tokens.push(self.readComment());
-            } else if ch == '"' || ch == '\''{
-                tokens.push(self.readString());
-            } else if is_operator(ch){
-                tokens.push(self.readOperator());
-            } else if ch.is_ascii_digit(){
-                tokens.push(self.readNumber());
-            } else if ch.is_alphabetic() || ch == '_'{
-                tokens.push(self.readWord())
-            } else {
-                self.advance();
-            }
-        }
-
-        tokens
-    }
-
-    fn is_operator(&self, ch: char) -> bool{
+    
+    pub fn is_operator(&self, ch: char) -> bool{
         matches!(ch, '+' | '-' | '*' | '/' | '%' | '=' | '<' | '>' | '!' | '&')
     }
 
     fn advance(&mut self){
-        self.currentPos += 1;
+        self.current_pos += 1;
     }
-
+    
     fn current(&self) -> Option<char>{
-        self.input.get(self.currentPos).copied()
+        self.input.get(self.current_pos).copied()
     }
-
+    
     fn peek(&self) -> Option<char>{
-        self.input.get(self.currentPos+1).copied()
+        self.input.get(self.current_pos+1).copied()
     }
-
-    fn readWhitespace(&mut self) -> Token {
+    
+    fn read_whitespace(&mut self) -> Token {
         let mut temp = String::new();
-
+    
         while let Some(c) = self.current() {
             if c.is_whitespace() {
                 if c == ' '{
                     temp.push_str("&nbsp;");
-
+    
                 } else if c == '\n' {
                     temp.push_str("<br>");
-
+    
                 } else if c == '\t' {
                     temp.push_str("&emsp;");
-
+    
                 }
                 self.advance();
-
+    
             } else {
                 break;
                 
             }
         }
-
+    
         Token::new(temp, TokenType::Whitespace)
     }
-
-    fn readComment(&mut self) -> Token{
+    
+    fn read_comment(&mut self) -> Token{
         let mut temp = String::new();
-
+    
         while let Some(c) = self.current() {
             if c == '\n' {
                 break;
@@ -91,25 +67,26 @@ impl Lexer{
                 self.advance();
             }
         }
+        Token::new(temp, TokenType::Comments)
     }
-
-    fn readString(&mut self) -> Token{
+    
+    fn read_string(&mut self) -> Token{
         unimplemented!() 
     }
-
-    fn readOperator(&mut self) -> Token{
+    
+    fn read_operator(&mut self) -> Token{
         unimplemented!() 
     }
-
-    fn readNumber(&mut self) -> Token{
+    
+    fn read_number(&mut self) -> Token{
         unimplemented!() 
     }
-
-    fn readWord(&mut self) -> Token{
+    
+    fn read_word(&mut self) -> Token{
         let mut temp = String::new();
-        let rWords = ["if", "else", "for", "while", "def", "return", "class", "import"];
-        let oWords = ["and", "or", "not", "in", "is"];
-
+        let r_words = ["if", "else", "for", "while", "def", "return", "class", "import"];
+        let o_words = ["and", "or", "not", "in", "is"];
+    
         while let Some(c) = self.current(){
             if c.is_alphabetic() || c == '_' || c.is_ascii_digit(){
                 temp.push(c);
@@ -118,14 +95,40 @@ impl Lexer{
                 break;
             }
         }
-
-        if rWords.contains(&temp.as_str()){
+    
+        if r_words.contains(&temp.as_str()){
             Token::new(temp, TokenType::Keyword)
-        } else if oWords.contains(&temp.as_str()){
+        } else if o_words.contains(&temp.as_str()){
             Token::new(temp, TokenType::Operator)
         } else {
             Token::new(temp, TokenType::Variable)
         }
+    }
+    
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        while let Some(ch) = self.current(){
+            if ch.is_whitespace(){
+                tokens.push(self.read_whitespace());
+            } else if ch == '#' {
+                tokens.push(self.read_comment());
+            } else if ch == '"' || ch == '\''{
+                tokens.push(self.read_string());
+            } else if self.is_operator(ch){
+                tokens.push(self.read_operator());
+            } else if ch.is_ascii_digit(){
+                tokens.push(self.read_number());
+            } else if ch.is_alphabetic() || ch == '_'{
+                tokens.push(self.read_word())
+            } else {
+                tokens.push(Token::new(ch.to_string(), TokenType::None));
+                self.advance();
+            }
+        }
+
+        tokens
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ mod html_generator;
 mod token;
 
 use lexer::{Lexer};
-use html_generator::{htmlGenerator, fileGenerator};
+use html_generator::{html_generator, file_generator};
 
-use crate::token::{Token, TokenType};
+// use crate::token::{Token, TokenType};
 
 fn main() -> std::io::Result<()>{
-    let python_code = String::from("def sum(x,y):\n return mama\n\t#pruebas de lectura");
+    let python_code = String::from("#frutas = ['manzana', 'plátano', 'mango']
+for fruta in frutas:
+    print(fruit.capitalize())
+
+# Output: Manzana
+#         Plátano
+#         Mango");
     let mut lexer = Lexer::new(python_code);
     let tokens = lexer.tokenize();
-    let html_content = htmlGenerator(tokens);
-    fileGenerator(html_content)?;
+    let html_content = html_generator(tokens);
+    file_generator(html_content)?;
     println!("Archivo htmlgenerado correctamente");
     Ok(())
 }
